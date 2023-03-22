675de6f018031da3529ea1b4f1a728ec474a6d17
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-22 17:38:01 -0400

    Added configuration files

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+export default {
+	clearMocks: true,
+	collectCoverage: true,
+	collectCoverageFrom: [
+		'src/**/*.ts',
+		'!src/**/index.ts',
+	],
+	coverageDirectory: 'tests/output/coverage',
+	coverageProvider: 'v8',
+	preset: 'ts-jest',
+	roots: ['src', 'tests'],
+	testEnvironment: 'jest-environment-node',
+	testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.tests.ts'],
+	verbose: true,
+	transform: {
+		'^.+\\.tsx?$': [
+			'ts-jest', { tsconfig: 'tsconfig.tests.json' }
+		]
+	}
+}
