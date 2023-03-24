9e2000af5850305b9254076a7a05363feddf3647
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-24 17:49:31 -0400

    Rewrote jest config to use root jest config

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,20 +1,18 @@
+
+import config from '../../jest.config'
+
 export default {
-	clearMocks: true,
-	collectCoverage: true,
+	...config,
 	collectCoverageFrom: [
 		'src/**/*.ts',
-		'!src/**/index.ts'
+		'!src/**/index.ts',
+		'!src/JsonObject.ts'
 	],
 	coverageDirectory: 'tests/output/coverage',
-	coverageProvider: 'v8',
-	preset: 'ts-jest',
 	roots: [
 		'src',
 		'tests'
 	],
-	testEnvironment: 'jest-environment-node',
-	testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.tests.ts'],
-	verbose: true,
 	transform: {
 		'^.+\\.tsx?$': [
 			'ts-jest', { tsconfig: '../../tsconfig.tests.json' }
