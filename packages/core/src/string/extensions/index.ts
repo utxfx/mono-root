483fbb8fa5c0c1af657aad9584f7a98d30683cb5
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-25 14:30:28 -0400

    Added string extensions barrel file

---
+++ packages/core/src/string/extensions/index.ts
@@ -0,0 +1,13 @@
+
+export * from './after'
+export * from './afterLast'
+export * from './before'
+export * from './beforeLast'
+export * from './json'
+export * from './lines'
+export * from './lookFor'
+export * from './lookForAll'
+export * from './lookForLast'
+export * from './splitOnce'
+export * from './sx'
+export * from './write'
