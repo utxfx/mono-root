3e888063fb42e8cd3eca4fab44ea00a95cba77c9
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-25 15:04:12 -0400

    Removed package tests from jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,56 +1,17 @@
 
-import { readdirSync, existsSync, lstatSync } from 'fs'
-
-const pkgFolder = 'packages'
-
-const isFile = (path: string) =>
-	existsSync(path) && lstatSync(path).isFile()
-
-const isFolder = (path: string) =>
-	existsSync(path) && lstatSync(path).isDirectory()
-
-let packages: string[] = []
-const oldCwd = process.cwd()
-process.chdir(__dirname)
-try{
-	packages = readdirSync(pkgFolder, { withFileTypes: true })
-		.filter(child =>
-			child.isDirectory()
-			&& isFile(`${pkgFolder}/${child.name}/package.json`)
-			&& isFile(`${pkgFolder}/${child.name}/tsconfig.json`)
-			&& isFolder(`${pkgFolder}/${child.name}/src`)
-			&& isFolder(`${pkgFolder}/${child.name}/tests`))
-		.map(child => child.name)
-}
-finally{
-	process.chdir(oldCwd)
-}
-
 export default {
 	clearMocks: true,
 	collectCoverage: true,
 	collectCoverageFrom: [
 		'src/**/*.ts',
-		'!src/**/index.ts',
-		...packages
-			.map(name => [
-				`${pkgFolder}/${name}/src/**/*.ts`,
-				`!${pkgFolder}/${name}/src/**/index.ts`
-			])
-			.flat()
+		'!src/**/index.ts'
 	],
 	coverageDirectory: 'tests/output/coverage',
 	coverageProvider: 'v8',
 	preset: 'ts-jest',
 	roots: [
 		'src',
-		'tests',
-		...packages
-			.map(name => [
-				`${pkgFolder}/${name}/src`,
-				`${pkgFolder}/${name}/tests`
-			])
-			.flat()
+		'tests'
 	],
 	testEnvironment: 'jest-environment-node',
 	testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.tests.ts'],
