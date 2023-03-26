daae162368d4d325ec7a2b0f3d5936b0f815701d
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-26 16:45:41 -0400

    Added barrel file to string

---
+++ packages/core/src/string/index.ts
@@ -0,0 +1,11 @@
+
+export * from './after'
+export * from './afterLast'
+export * from './before'
+export * from './beforeLast'
+export * from './json'
+export * from './lines'
+export * from './lookFor'
+export * from './lookForLast'
+export * from './splitOnce'
+export * from './write'
