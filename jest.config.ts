bd93d9419ca8b304b04e1b0e620a60da97eb8db7
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-27 17:34:50 -0400

    Added packages back into root jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,41 @@
 
+import { packages } from './src/packages'
+
+let pkgFolders: string[] = []
+const oldCwd = process.cwd()
+process.chdir(__dirname)
+try{
+	pkgFolders = packages().map(pkg => pkg.location)
+}
+finally{
+	process.chdir(oldCwd)
+}
+
 export default {
 	clearMocks: true,
 	collectCoverage: true,
 	collectCoverageFrom: [
 		'src/**/*.ts',
-		'!src/**/index.ts'
+		'!src/**/index.ts',
+		...pkgFolders
+			.map(folder => [
+				`${folder}/src/**/*.ts`,
+				`!${folder}/src/**/index.ts`
+			])
+			.flat()
 	],
 	coverageDirectory: 'tests/output/coverage',
 	coverageProvider: 'v8',
 	preset: 'ts-jest',
 	roots: [
 		'src',
-		'tests'
+		'tests',
+		...pkgFolders
+			.map(folder => [
+				`${folder}/src`,
+				`${folder}/tests`
+			])
+			.flat()
 	],
 	testEnvironment: 'jest-environment-node',
 	testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.tests.ts'],
