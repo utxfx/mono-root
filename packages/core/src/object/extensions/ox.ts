d8e8ad30ba163acf57158d6c4c427d39dc5895c6
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-31 10:29:15 -0400

    ObjectX allows undefined

--- packages/core/src/object/extensions/ox.ts
+++ packages/core/src/object/extensions/ox.ts
@@ -5,24 +5,30 @@ type AnyFn = (...args: unknown[]) => unknown
 export type Extensible = boolean | number | string | object | AnyFn
 
 export class ObjectX<T> extends Object implements Extension<T> {
-	constructor(readonly value: T) {
-		super(value)
+	constructor(private readonly _value?: T) {
+		super(_value)
 	}
 
-	$v(): T { return this.value }
+	$v(): T { 
+		if (typeof this._value == 'object' && this._value == null)
+			throw new Error('ObjectX: value is null object')
+		if (this._value == undefined)
+			throw new Error('ObjectX: value is undefined')
+		return this._value
+	}
 
-	valueOf(): object {	return this.value as object	}
-	toString(): string { return this.value?.toString() ?? '' }
+	valueOf(): object {	return this._value as object	}
+	toString(): string { return this._value?.toString() ?? '' }
 	hasOwnProperty(prop: string | number | symbol): boolean { 
 		// eslint-disable-next-line no-prototype-builtins
-		return this.value?.hasOwnProperty(prop) ?? false
+		return this._value?.hasOwnProperty(prop) ?? false
 	}
 
 	log() {
-		console.log(this.value)
+		console.log(this._value)
 		return this
 	}
 }
 
-export const ox = <T = object>(value: T) =>
+export const ox = <T = object>(value?: T) =>
 	new ObjectX<T>(value)
