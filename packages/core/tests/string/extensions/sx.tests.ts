9864bb392062db290b19cea23c54808950a198ad
Sam Freeman <sam.freeman.55@gmail.com>
2023-03-31 10:32:31 -0400

    Added tests for sx.$v

--- packages/core/tests/string/extensions/sx.tests.ts
+++ packages/core/tests/string/extensions/sx.tests.ts
@@ -5,5 +5,9 @@ describe('@utxfx/core', () => {
 	test('sx', () => {
 		expect(sx().toString()).toBe('')
 		expect(''.$x().toString()).toBe('')
+		expect(sx('abc').toString()).toBe('abc')
+		expect('abc'.$x().toString()).toBe('abc')
+		expect(sx('abc').$v()).toBe('abc')
+		expect('abc'.$x().$v()).toBe('abc')
 	})
 })
